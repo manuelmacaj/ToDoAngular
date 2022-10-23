3b40a4ad4daca6ddef949999b5adcf45562cfa2e
manuelmacaj <49785644+manuelmacaj@users.noreply.github.com>
2022-10-23 22:05:19 +0200

    add flex layout to show own todo

--- src/app/ToDos/shows-to-do/shows-to-do.component.ts
+++ src/app/ToDos/shows-to-do/shows-to-do.component.ts
@@ -1,3 +1,4 @@
+import { TmplAstBoundAttribute } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/auth.service';
 import { ToDoElem } from 'src/app/Interfaces/toDoInterface';
@@ -17,20 +18,40 @@ export class ShowsToDoComponent implements OnInit {
   ngOnInit(): void {
     this.getAllToDoList();
   }
-  
+
   // https://www.djamware.com/post/5d8d7fc10daa6c77eed3b2f2/angular-8-tutorial-rest-api-and-httpclient-examples
   // https://mercyjemosop.medium.com/rest-api-with-angular-e7c2ceaaace1
 
   getAllToDoList() { // funzione per il prelevamento di tutti gli elementi dal database
     this.toDoService.getAllToDoList().subscribe({
-      next: (res: any) => this.showToDos(res), 
+      next: (res: any) => this.showToDos(res),
       error: _ => this.auth.logout("Tentativo di lettura dei To-Do fallita, rieffettuare il login")
     });
   }
-  protected showToDos (data: any) {
+
+  protected showToDos(data: any) {
     this.toDoList = data
-      this.toDoList.reverse();
-  } 
+    //this.toDoList.reverse();
+  }
+
+  protected notCompletedToDo(): ToDoElem[] {
+    var taskNotCompleted: ToDoElem[] = []
+    this.toDoList.forEach((element) => {
+      if (element.fatto == false)
+        taskNotCompleted.push(element)
+    })
+    return taskNotCompleted;
+  }
+
+  protected completedToDo() {
+    var taskNotCompleted: ToDoElem[] = []
+    this.toDoList.forEach((element) => {
+      if (element.fatto == true)
+        taskNotCompleted.push(element)
+    })
+    return taskNotCompleted;
+  }
+
   logout() {
     this.auth.logout("Logout completato.");
   }
